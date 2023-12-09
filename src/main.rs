6d68cd38905c89d1d1b7c0d98b8e105ac5298eb7
neo <chojs990222@gmail.com>
2023-12-09 23:20:33 +0900

    DNS struct refactor

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod protocol;
 
 use std::net::UdpSocket;
 
+use crate::protocol::DNS;
+
 fn main() {
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
@@ -11,14 +13,7 @@ fn main() {
             Ok((size, source)) => {
                 let _received_data = String::from_utf8_lossy(&buf[0..size]);
                 println!("Received {} bytes from {}", size, source);
-                let header = protocol::header::DNSHeader::parse(&buf);
-                println!("Header: {:?}", header);
-                let question = protocol::question::DNSQuestion::parse(&buf, 12, header.qdcount);
-                println!("Question: {:?}", question);
-                let mut response: Vec<u8> = Vec::new();
-                let response_header = protocol::header::DNSHeader::new(header.id, true, 1);
-                response.extend_from_slice(&response_header.to_bytes());
-                response.extend_from_slice(&question.0.to_bytes());
+                let response: Vec<u8> = DNS::parse(&buf[0..size]).response();
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,2 +1,30 @@
 pub mod header;
 pub mod question;
+
+pub struct DNS {
+    header: header::DNSHeader,
+    questions: Vec<question::DNSQuestion>,
+}
+
+impl DNS {
+    pub fn parse(bytes: &[u8]) -> Self {
+        let header = header::DNSHeader::parse(bytes);
+        let mut questions = Vec::new();
+        let mut start = 12;
+        for i in 0..header.qdcount {
+            let (question, next) = question::DNSQuestion::parse(bytes, start, header.qdcount - i);
+            questions.push(question);
+            start = next;
+        }
+        DNS { header, questions }
+    }
+
+    pub fn response(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+        bytes.extend_from_slice(&self.header.to_bytes());
+        for question in &self.questions {
+            bytes.extend_from_slice(&question.to_bytes());
+        }
+        bytes
+    }
+}
